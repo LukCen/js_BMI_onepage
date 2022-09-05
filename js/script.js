6d79ceec7cf04a455276fc72625f19c64567c4d7
Lukasz Cena <lbjcena@gmail.com>
2022-09-05 18:55:04 +0200

    minor refactors

--- js/script.js
+++ js/script.js
@@ -1,41 +1,50 @@
 'use strict';
 
-let userWeight = document.querySelector('.weight').value;
-let userHeight = document.querySelector('.height').value;
+const userWeight = parseInt(document.getElementById('weight').value);
+const userHeight = parseInt(document.getElementById('height').value);
 
-let btnSubmit = document.querySelector('.btn-submit');
+const btnSubmit = document.getElementById('btn-submit');
 
-let resultField = document.querySelector('.result-window');
+const resultField = document.getElementById('result-window');
 
+resultField.innerText = 'Twoje BMI wynosi: '
 
 
 
 btnSubmit.addEventListener('click', () => {
+    
+    // resultField.innerText = '';
 
-    let bmiWeight = parseFloat(userWeight);
-    let bmiHeight = parseFloat(userHeight);
-    let bmiResult = bmiWeight / (Math.pow(bmiHeight, 2) / 100);
+    // const bmiWeight = userWeight;
+    // const bmiHeight = userHeight;
+    const bmiResult = userWeight / (Math.pow(userHeight), 2 / 100);
 
 
     let finalResult = bmiResult * 100;
 
-    let floatResult = parseFloat(finalResult)
-        // resultField.textContent = finalResult.toFixed(1);
-    if(floatResult < 16){
-        resultField.textContent = 'Twoje BMI wynosi: ' + floatResult.toFixed(2) + '. ' + 'Jesteś wygłodzony!'
+    let floatResult = parseFloat(finalResult);
 
-    } else if(floatResult === 16 || floatResult < 17){
-        resultField.textContent = 'Twoje BMI wynosi: ' + floatResult.toFixed(2) + '. ' + 'Jesteś wychudzony!'
+    console.log(floatResult);
 
-    } else if(floatResult === 17 || floatResult < 18.50) {
-        resultField.textContent = 'Twoje BMI wynosi: ' + floatResult.toFixed(2) + '. ' + 'Cierpisz na niedowagę!'
+    
+    // if(floatResult < 16){
+    //     resultField.textContent += floatResult + '. ' + 'Jesteś wygłodzony!'
 
-    } else if(floatResult === 18.50 || floatResult < 25) {
-        resultField.textContent = 'Twoje BMI wynosi: ' + floatResult.toFixed(2) + '. ' + 'Jesteś w dobrym miejscu, tak trzymać! To prawidłowa wartość BMI!'
-    } else if (floatResult === 25 || floatResult < 29.99) {
-        resultField.textContent = 'Twoje BMI wynosi: ' + floatResult.toFixed(2) + '. ' + 'Cierpisz na nadwagę!'
-    } else {
-        resultField.textContent = 'Twoje BMI wynosi: ' + floatResult.toFixed(2) + '. ' + 'Cierpisz na otyłość!'
-    }
+    // } else if(floatResult > 16 && floatResult < 17) {
+    //     resultField.textContent += floatResult + '. ' + 'Jesteś wychudzony!'
+
+    // } else if(floatResult > 17 && floatResult < 18.50) {
+    //     resultField.textContent += floatResult + '. ' + 'Cierpisz na niedowagę!'
+
+    // } else if(floatResult > 18.50 && floatResult < 25) {
+    //     resultField.textContent += floatResult + '. ' + 'Jesteś w dobrym miejscu, tak trzymać! To prawidłowa wartość BMI!'
+
+    // } else if (floatResult > 25 && floatResult < 29.99) {
+    //     resultField.textContent += floatResult + '. ' + 'Cierpisz na nadwagę!'
+
+    // } else {
+    //     resultField.textContent += floatResult + '. ' + 'Cierpisz na otyłość!'
+    // }
+
 
 })
