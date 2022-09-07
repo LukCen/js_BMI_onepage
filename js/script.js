c3faaa583b2c9a71664c24361935b74b73a2a235
Lukasz Cena <lbjcena@gmail.com>
2022-09-07 18:53:45 +0200

    code refactor and clean-up

--- js/script.js
+++ js/script.js
@@ -2,43 +2,45 @@
 
     
  
-const btnSubmit = document.getElementById('btn-submit');
-const resultField = document.getElementById('result-window');
+const btnSubmit = document.querySelector('#btn-submit');
+const resultField = document.querySelector('#result-window');
 
 btnSubmit.addEventListener('click', () => {
 
-    let userWeight = document.getElementById('weight').value;
-    let userHeight = document.getElementById('height').value;
+    let userHeight = 0;
+    let userWeight = 0;
+
+    userWeight = document.querySelector('#weight').value;
+    userHeight = document.querySelector('#height').value;
 
     const bmiResult = userWeight / (userHeight*userHeight / 100);
 
-    const finalResult = " " + parseFloat(bmiResult * 100).toFixed(2);
+    const finalResult = ` ${parseFloat(bmiResult * 100).toFixed(2)}`;
 
    
 
     resultField.innerText = 'Twoje BMI wynosi: ' 
 
     if(finalResult < 16){
-        resultField.innerText += finalResult + '. ' + ' Jesteś wygłodzony!'
+        resultField.innerText += `${finalResult}. Jesteś wygłodzony!`
 
     } else if(finalResult > 16 && finalResult < 17) {
-        resultField.innerText += finalResult + '. ' + ' Jesteś wychudzony!'
+        resultField.innerText += `${finalResult}. Jesteś wychudzony!`
 
     } else if(finalResult > 17 && finalResult < 18.50) {
-        resultField.innerText += finalResult + '. ' + ' Cierpisz na niedowagę!'
+        resultField.innerText += `${finalResult}. Cierpisz na niedowagę!`
 
     } else if(finalResult > 18.50 && finalResult < 25) {
-        resultField.innerText += finalResult + '. ' + ' Jesteś w dobrym miejscu, tak trzymać! To prawidłowa wartość BMI!'
+        resultField.innerText += `${finalResult}. Jesteś w dobrym miejscu, tak trzymać! To prawidłowa wartość BMI!'`
 
     } else if (finalResult > 25 && finalResult < 29.99) {
-        resultField.innerText += finalResult + '. ' + ' Cierpisz na nadwagę!'
+        resultField.innerText += `${finalResult}. Cierpisz na nadwagę!`
 
     } else {
-        resultField.innerText += finalResult + '. ' + ' Cierpisz na otyłość!'
+        resultField.innerText += `${finalResult}. Cierpisz na otyłość!`
     }
 
-    userHeight = 0;
-    userWeight = 0;
+ 
 
 })
 
